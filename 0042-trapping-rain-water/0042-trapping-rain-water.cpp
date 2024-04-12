76b6ae6b3bed662fa6913e186b776a2cc046818f
Navneet Kumar <100843177+Navneet0801@users.noreply.github.com>
2024-04-12 11:58:13 +0530

    Time: 11 ms (54.11%), Space: 23.5 MB (7.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,52 +1,42 @@
 class Solution {
 public:
-    // void prevHighest(vector<int> &height, vector<int> &left) {
-    //     int n = height.size();
-    //     left.resize(n, 0);
-    //     int maxi = 0;       
-    //     for(int i=0; i<n-1; i++) {
-    //         left[i] = maxi;
-    //         maxi = max(maxi, height[i]);
-    //     }
-    // }
-    // void nextHighest(vector<int> &height, vector<int> &right) {
-    //     int n = height.size();
-    //     right.resize(n, 0);
-    //     int maxi = 0;
-    //     for(int i=n-1; i>0; i--) {
-    //         right[i] = maxi;
-    //         maxi = max(maxi, height[i]);
-    //     }
-    // }
-    int trap(vector<int>& height) {
-//         vector<int> left;
-//         vector<int> right;
+    int n;
+    void prevMaxElement(vector<int> &height, vector<int> &leftPrev) {
+        leftPrev.resize(n, 0);
+        int maxi = 0;
         
-//         prevHighest(height, left);
-//         nextHighest(height, right);
-        
-//         int n = height.size();
-//         vector<int> water(n, -1);
-//         for(int i=1; i<n-1; i++) 
-//             water[i] = min(left[i], right[i]) - height[i];
-        
-//         int ans = 0;
-//         for(int i: water) {
-//             if(i > 0)
-//                 ans += i;
-//         }
-//         return ans;
-        int l = 0;
-        int r = height.size()-1;
-        int lmax = INT_MIN;
-        int rmax = INT_MIN;
-        int ans = 0;
-        
-        while(l < r){
-            lmax = max(lmax, height[l]);
-            rmax = max(rmax, height[r]);
-            ans += (lmax < rmax) ? lmax-height[l++] : rmax-height[r--];
+        for(int i=0; i<n-1; i++) {
+            leftPrev[i] = maxi;
+            maxi = max(maxi, height[i]);
         }
+    }
+    void nextMaxElement(vector<int> &height, vector<int> &rightNext) {
+        rightNext.resize(n, 0);
+        int maxi = 0;
+        
+        for(int i=n-1; i>0; i--) {
+            rightNext[i] = maxi;
+            maxi = max(maxi, height[i]);
+        }
+    }
+    int trap(vector<int>& height) {
+        n = height.size();
+        vector<int> leftPrev;
+        vector<int> rightNext;
+        
+        prevMaxElement(height, leftPrev);
+        nextMaxElement(height, rightNext);
+        
+        vector<int> water(n, 0);
+        for(int i=0; i<n; i++) 
+            water[i] = min(leftPrev[i], rightNext[i]) - height[i];
+        
+        int ans = 0;
+        for(int i: water) {
+            if(i > 0)
+                ans += i;
+        }
+        
         return ans;
     }
 };
